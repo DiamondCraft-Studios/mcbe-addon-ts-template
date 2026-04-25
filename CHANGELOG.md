f260f2284273efe040699f1277388b0e5d960f52
deviank <tensaidevi@gmail.com>
2026-04-25 14:09:42 +0800

    Add changelog automation script

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+## 1.0.0 (2026-04-25)
+
+
+## 📦 Uncategorized
+
+- Add .env template
+- Add folder structure
+- Add starter script
+- Add template pack folders
+- Add texts
+- Include empty .env
+- Initial commit
+- Update .gitignore to include .env
+- Update dev dependency
+- Update just scripts
+- Update package.json to latest modules
+- Update rp manifest to use translatable fields
---
+++ changelog.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+    parserOpts: {
+        headerPattern: /^(\w+)(?:\(([^)]+)\))?:\s(.+)$/,
+        headerCorrespondence: ["type", "scope", "subject"] ,
+        headerPartial: `# {{version}}\n`,
+        footerPartial: `\n\n**Full Changelog:** https://github.com/OWNER/REPO/compare/{{previousTag}}...{{currentTag}}`,
+    },
+
+    writerOpts: {
+        transform: (commit) => {
+            const headerMap = {
+                build: "🏗 Builds",
+                dev: "💻 Development",
+                model: "🧱 Models",
+                asset: "🎨 Assets",
+                design: "🧠 Design",
+                qa: "🧪 QA",
+            };
+
+            const normalizedType = commit.type?.toLowerCase();
+            const mappedType = headerMap[normalizedType];
+            let subject = commit.subject || commit.header || commit.message;
+            subject = subject.trim().charAt(0).toUpperCase() + subject.slice(1);
+
+            return {
+                ...commit,
+                subject: subject || "No message",
+                type: mappedType || "📦 Uncategorized",
+            };
+        },
+
+        groupBy: "type",
+
+        commitGroupsSort: (a, b) => a.title.localeCompare(b.title),
+        commitsSort: ["subject"],
+
+        mainTemplate: `
+{{> header}}
+
+{{#each commitGroups}}
+
+## {{title}}
+
+{{#each commits}}
+- {{subject}}
+{{/each}}
+
+{{/each}}
+
+{{> footer}}
+`,
+    },
+};
